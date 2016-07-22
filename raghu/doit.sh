8d607050da50699d1f66201010d5a3b51db4bd00
sdnlab <arun.abhinay@gmail.com>
2016-07-22 23:31:46 +0530

    rghu white db

---
+++ raghu/doit.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export RTE_SDK=/home/Innovhyd/raghu/dpdk-16.04
+export RTE_TARGET=x86_64-native-linuxapp-gcc
+
+export LD_LIBRARY_PATH=/usr/local/lib
+PATH=$PATH:/home/Innovhyd/raghu/whitedb-0.7.3:/home/Innovhyd/raghu/dpdk-16.04
+
---
+++ raghu/dpdk-16.04/doit.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export RTE_SDK=/home/Innovhyd/raghu/dpdk-16.04
+export RTE_TARGET=x86_64-native-linuxapp-gcc
